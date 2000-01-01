0a0543a76ff2989d070693490b78625657a6c684
Alejandro Gonzalez <a-gonzalez@ti.com>
1999-12-31 18:03:26 -0600

    videoenc: Use shared buffers on output port.
    Acked-by: Kiran Nataraju <knataraju@ti.com>
    Signed-off-by: Daniel Diaz <ddiaz@ti.com>

--- omx/gstomx_base_videoenc.c
+++ omx/gstomx_base_videoenc.c
@@ -332,9 +332,9 @@ type_instance_init (GTypeInstance *instance,
     omx_base->omx_setup = omx_setup;
 
     omx_base->in_port->omx_allocate = FALSE;
-    omx_base->out_port->omx_allocate = TRUE;
+    omx_base->out_port->omx_allocate = FALSE;
     omx_base->in_port->share_buffer = TRUE;
-    omx_base->out_port->share_buffer = FALSE;
+    omx_base->out_port->share_buffer = TRUE;
 
     gst_pad_set_setcaps_function (omx_base->sinkpad, sink_setcaps);
 
